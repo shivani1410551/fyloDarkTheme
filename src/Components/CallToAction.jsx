4b55feb54f0cceee029185dcb1ede3561430822f
shivani charan <sushikim2050@gmail.com>
2024-08-07 16:09:19 +0530

    add landmark roles

--- src/Components/CallToAction.jsx
+++ src/Components/CallToAction.jsx
@@ -8,10 +8,8 @@ const CallToAction = () => {
       .required("Required"),
   });
   return (
-    <main>
-      {" "}
-      <section
-        className="CallToAction font-primary bg-DarkBlue1 rounded-lg p-4  xxs:my-6  xxs:mx-8
+    <section
+      className="CallToAction font-primary bg-DarkBlue1 rounded-lg p-4  xxs:my-6  xxs:mx-8
   mx-auto xxs:p-4
   xs:p-6 sm:p-8 md:p-8 grid xxs:grid-cols-1 text-slate-200 
   xxs:space-y-4 text-center
@@ -26,41 +24,41 @@ const CallToAction = () => {
 xxs:left-[0.15rem]
 xxs:right-5
   "
+    >
+      <h2 className="font-bold xxs:text-sm   font-secondary md:text-2xl">
+        Get early access today
+      </h2>
+      <p className="xxs:text-[0.65rem] xxs:leading-5">
+        {" "}
+        It only takes a minute to sign up and our free starter tier is extremely
+        generous. If you have any questions, our support team would be happy to
+        help you.
+      </p>
+      <Formik
+        initialValues={{ email: "" }}
+        onSubmit={(value, resetForm) => {
+          console.log(value.email);
+          resetForm();
+        }}
+        validationSchema={formSchema}
       >
-        <h2 className="font-bold xxs:text-sm   font-secondary md:text-2xl">
-          Get early access today
-        </h2>
-        <p className="xxs:text-[0.65rem] xxs:leading-5">
-          {" "}
-          It only takes a minute to sign up and our free starter tier is
-          extremely generous. If you have any questions, our support team would
-          be happy to help you.
-        </p>
-        <Formik
-          initialValues={{ email: "" }}
-          onSubmit={(value, resetForm) => {
-            console.log(value.email);
-            resetForm();
-          }}
-          validationSchema={formSchema}
-        >
-          <Form className="xxs:space-y-2  md:grid md:grid-cols-[3fr_1fr] md:gap-x-6">
-            <Field
-              type="email"
-              id="email"
-              name="email"
-              placeholder="email@example.com"
-              className="   block xxs:w-full shadow-md
+        <Form className="xxs:space-y-2  md:grid md:grid-cols-[3fr_1fr] md:gap-x-6">
+          <Field
+            type="email"
+            id="email"
+            name="email"
+            placeholder="email@example.com"
+            className="   block xxs:w-full shadow-md
         mx-auto text-justify text-sm my-4 font-bold rounded-3xl placeholder:text-slate-300  outline-none xxs:py-2 indent-4
         md:w-[400px]
         xxs:placeholder:text-xs
         text-black
         "
-            />
+          />
 
-            <button
-              type="submit"
-              className="font-bold rounded-3xl bg-gradient-to-r from-Cyan to-Blue
+          <button
+            type="submit"
+            className="font-bold rounded-3xl bg-gradient-to-r from-Cyan to-Blue
           shadow-medium xxs:w-[100%] 
         mx-auto text-center text-sm 
         md:w-[10rem]
@@ -73,14 +71,13 @@ xxs:right-5
         md:justify-center
         xxs:text-xs
         "
-            >
-              Get Started For Free
-            </button>
-            <ErrorMessage name="email" component="div" className="msg" />
-          </Form>
-        </Formik>
-      </section>
-    </main>
+          >
+            Get Started For Free
+          </button>
+          <ErrorMessage name="email" component="div" className="msg" />
+        </Form>
+      </Formik>
+    </section>
   );
 };
 
