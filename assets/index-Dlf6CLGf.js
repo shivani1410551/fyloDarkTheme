87db6ad644231b24005b5d30a3c607bd52aa21da
shivani1410551 <shivani1410551@users.noreply.github.com>
2024-08-07 10:22:47 +0000

    deploy: d319cd705f3552b8bd730cde4d03bcc594a2a22a

--- assets/index-Dlf6CLGf.js
+++ assets/index-Dlf6CLGf.js
@@ -83,7 +83,7 @@ attempted value: ${o}
     z-40
   xxs:left-[0.15rem]
   xxs:right-5
-    `,children:[v.jsx("h1",{className:"font-bold xxs:text-sm   font-secondary md:text-2xl",children:"Get early access today"}),v.jsxs("p",{className:"xxs:text-[0.65rem] xxs:leading-5",children:[" ","It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you."]}),v.jsx(yx,{initialValues:{email:""},onSubmit:(t,n)=>{console.log(t.email),n()},validationSchema:e,children:v.jsxs(Wp,{className:"xxs:space-y-2  md:grid md:grid-cols-[3fr_1fr] md:gap-x-6",children:[v.jsx(kx,{type:"email",id:"email",name:"email",placeholder:"email@example.com",className:`   block xxs:w-full shadow-md
+    `,children:[v.jsx("h2",{className:"font-bold xxs:text-sm   font-secondary md:text-2xl",children:"Get early access today"}),v.jsxs("p",{className:"xxs:text-[0.65rem] xxs:leading-5",children:[" ","It only takes a minute to sign up and our free starter tier is extremely generous. If you have any questions, our support team would be happy to help you."]}),v.jsx(yx,{initialValues:{email:""},onSubmit:(t,n)=>{console.log(t.email),n()},validationSchema:e,children:v.jsxs(Wp,{className:"xxs:space-y-2  md:grid md:grid-cols-[3fr_1fr] md:gap-x-6",children:[v.jsx(kx,{type:"email",id:"email",name:"email",placeholder:"email@example.com",className:`   block xxs:w-full shadow-md
           mx-auto text-justify text-sm my-4 font-bold rounded-3xl placeholder:text-slate-300  outline-none xxs:py-2 indent-4
           md:w-[400px]
           xxs:placeholder:text-xs
